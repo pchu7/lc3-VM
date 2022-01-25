56873b5f35414ef4aecbc8d8fd4d018e908bafeb
Phillip <philliprchu@gmail.com>
2022-01-25 17:41:19 -0500

    Added some more operations

--- lc3-vm.c
+++ lc3-vm.c
@@ -124,20 +124,60 @@ int main(int argc, const char* argv())
 					/* If the immediate bit is set */
 					if ( imm_flag )
 					{
+						/* Sign extend the immediate value */
 						uint16_t imm5 = sign_extend(instr & 0x1f, 5);
+						/* Add the two values */
 						reg[r0] = reg[r1] + imm5;
 					}
 					else {
+						/* Add value loaded from register */
 						uint16_t r2 = instr & 0x7;
 						reg[r0] = reg[r1] + reg[r2];
 					}
 					update_flags( r0 );
-					break;
 
 				case OP_AND:
+					/* Grab dest register */
+					uint16_t r0 = ( instr >> 9 ) & 0x7;
+					/* Grab first operand */
+					uint16_t r1 = ( instr >> 6 ) & 0x7;
+
+					uint16_t imm_flag = ( instr >> 5 ) & 0x1;
+
+					if ( imm_flag )
+					{
+
+						/* Sign extend the immediate value */
+						uint16_t imm5 = sign_extend(instr & 0x1f, 5);
+						reg[r0] = reg[r1] & imm5;
+					}
+					else
+					{
+						uint16_t r2 = instr & 0x7;
+						reg[r0] = reg[r1] & reg[r2];
+					}
+					update_flags( r0 );
+					
 				case OP_NOT:
+					/* Grab dest register */
+					uint16_t r0 = ( instr >> 9 ) & 0x7;
+					/* Grab first operand */
+					uint16_t r1 = ( instr >> 6 ) & 0x7;
+
+					reg[r0] = -reg[r1];
+					update_flags( r0 );
+
 				case OP_BR:
+					/* Grab pc offset */
+					uint16_t pc_offset = sign_extend(instr & 0x1ff, 9);
+					uint16_t cond_flag = ( instr >> 9 ) & 0x7;
+					if ( cond_flag & reg[R_COND] )
+					{
+						reg[R_PC] += pc_offset;
+					}
 				case OP_JMP:
+					uint16_t r1 = ( instr >> 6 ) & 0x7;
+					reg[R_PC] = reg[r1];
 				case OP_JSR:
 				case OP_LD:
 				case OP_LDI:
@@ -148,7 +188,6 @@ int main(int argc, const char* argv())
 					/* Add pc_offset to current PC and read from resulting address */
 					reg[r0] = mem_read( mem_read(reg[R_PC] + pc_offset ) );
 					update_flags( r0 );
-					break;
 
 				case OP_LDR:
 				case OP_LEA:
